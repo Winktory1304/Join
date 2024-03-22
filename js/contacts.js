60c1cd1434fa43781b7dba5748d5ddd9c102fb7b
Winktory1304 <thomaswinkler88@googlemail.com>
2024-03-22 11:32:39 +0100

    modal slindIn building

--- js/contacts.js
+++ js/contacts.js
@@ -1,17 +1,21 @@
-let modal = document.getElementById('addContactModal');
+let modal = document.getElementById('contactModal');
 let btn = document.getElementById('addContactBtn');
 let span = document.getElementsByClassName('close')[0];
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
 function addContactModal() {
-    modal.style.display = "block";
     
+    modal.style.display = "block";
+
 }
+
 
 function closeModal() {
     modal.style.display = "none";
+}
+
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        closeModal();
+    }
 }
