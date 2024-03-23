189b6705db72ae4419eb04ff411351c0776dacd1
Christina Bernhardt <sozia76@web.de>
2024-03-23 13:41:00 +0100

    js

---
+++ js/add_task.js
@@ -0,0 +1,25 @@
+let todos = [
+  {
+    id: 0,
+    tag: "User Story",
+    title: "Titel aus task",
+    subtasksdone: [],
+    subtasks: [],
+    priority: 0,
+    contacts: [],
+    category: "open",
+  },
+  {
+    id: 0,
+    tag: "User Story",
+    title: "Titel aus task",
+    subtasksdone: [],
+    subtasks: [],
+    priority: 0,
+    contacts: [],
+    category: "open",
+  },
+];
+
+// let todos = [];
+
