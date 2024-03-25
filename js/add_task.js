5486c57fddca7386b6d6b5c7f10ea29590d150c0
Christina Bernhardt <sozia76@web.de>
2024-03-25 21:15:19 +0100

    clear inputfields

--- js/add_task.js
+++ js/add_task.js
@@ -1,15 +1,13 @@
-let todos = [
-  {
-    id: 0,
-    tag: "User Story",
-    title: "Hier steht der Titel",
-    task: "Hier wird die Task stehen",
-    subtasksdone: [0, 1],
-    subtasks: ["subtask1", "subtask2"],
-    date: "08/08/2024",
-    priority: 0,
-    contacts: ["Max Mustermann"],
-    category: "open",
+let todos = [{
+  'id': 0,
+  'tag': 'User Story',
+  'title': 'Hier steht der Titel',
+  'task': 'Hier wird der Taskname stehen',
+  'subtasksdone': [],
+  'subtasks': [],
+  'priority': 0,
+  'contacts': [],
+  'category': 'open'
   },
   {
     id: 1,
