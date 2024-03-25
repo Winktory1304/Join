0d986e810f7fb362f6329ce6505a0f9e45891eee
Christina Bernhardt <sozia76@web.de>
2024-03-25 17:34:53 +0100

    Style

--- js/add_task.js
+++ js/add_task.js
@@ -1,51 +1,65 @@
 let todos = [
   {
-    'id': 0,
-    'tag': 'User Story',
-    'title': 'Hier steht der Titel',
-    'task': 'Hier wird die Task stehen',
-    'subtasksdone': [0, 1],
-    'subtasks': ['subtask1', 'subtask2'],
-    'date': '08/08/2024',
-    'priority': 0,
-    'contacts': ['Max Mustermann'],
-    'category': 'open'
+    id: 0,
+    tag: "User Story",
+    title: "Hier steht der Titel",
+    task: "Hier wird die Task stehen",
+    subtasksdone: [0, 1],
+    subtasks: ["subtask1", "subtask2"],
+    date: "08/08/2024",
+    priority: 0,
+    contacts: ["Max Mustermann"],
+    category: "open",
   },
   {
-    'id': 1,
-    'tag': 'User Story',
-    'title': 'Hier steht der Titel',
-    'task': 'Hier wird die Task stehen',
-    'subtasksdone': [0, 1],
-    'subtasks': ['subtask1', 'subtask2'],
-    'date': '08/08/2024',
-    'priority': 0,
-    'contacts': ['Max Mustermann'],
-    'category': 'open'
+    id: 1,
+    tag: "User Story",
+    title: "Hier steht der Titel",
+    task: "Hier wird die Task stehen",
+    subtasksdone: [0, 1],
+    subtasks: ["subtask1", "subtask2"],
+    date: "08/08/2024",
+    priority: 0,
+    contacts: ["Max Mustermann"],
+    category: "open",
   },
 ];
 
 // let todos = [];
 
 function getItems() {
-  todos.push(getItems(todos))
+  todos.push(getItems(todos));
 }
 /**
  * This function is used to validate the date
- * 
+ *
  */
-function validateDate(date) {
-  const regex = /^\d{4}-\d{2}-\d{2}$/; // Regex pattern for YYYY-MM-DD format
-  if (!regex.test(date)) {
-    console.log("Invalid date format. Please enter a date in the format YYYY-MM-DD.");
-    return false;
-  }
 
-  const inputDate = new Date(date);
-  if (isNaN(inputDate.getTime())) {
-    console.log("Invalid date. Please enter a valid date.");
-    return false;
-  }
+function validateDate(dateString) {
+  // Validates that the input string is a valid date formatted as "dd/mm/yyyy"
+  // function isValidDate(dateString)
+  // {
+  // First check for the pattern
+  console.log("klappt", dateString);
+  if (!/^\d{4}-\d{1,2}-\d{1,2}$/.test(dateString)) return false;
 
-  return true;
+  // Parse the date parts to integers
+  var parts = dateString.split("/");
+  var day = parseInt(parts[2], 10);
+  var month = parseInt(parts[1], 10);
+  var year = parseInt(parts[0], 10);
+
+  // Check the ranges of month and year
+  if (year < 2024 || year > 2100 || month == 0 || month > 12)
+    {console.log("falsches Datum", dateString);
+      return false;}
+
+  var monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+  // Adjust for leap years
+  if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+    monthLength[1] = 29;
+
+  // Check the range of the day
+  return day > 0 && day <= monthLength[month - 1];
 }
