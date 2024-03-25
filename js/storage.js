90c6b9d4eb3c9b96273be23d8ec457a32c48c9c4
Winktory1304 <thomaswinkler88@googlemail.com>
2024-03-25 19:04:15 +0100

    Revert "Squashed commit of the following:"
    This reverts commit 7c916a9be2a9b734697b43ba03de7e5c38cf62ca.

---
+++ js/storage.js
@@ -0,0 +1,14 @@
+const STORAGE_TOKEN = '8STE96HUTZL9YKNUBSW9RNRPEO8TPI1FQYJ3XUBI';
+const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
+
+
+async function setItem(key, value) {
+    const payload = { key, value, token: STORAGE_TOKEN };
+    return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload)})
+    .then(res => res.json());
+}
+
+async function getItem(key) {
+    const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
+    return fetch(url).then(res => res.json());
+}
