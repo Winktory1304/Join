b9850bbf93d47fef1cfd06f4df249de8e8ab0be8
Winktory1304 <thomaswinkler88@googlemail.com>
2024-03-25 15:56:36 +0100

    add contacts

--- js/contacts.js
+++ js/contacts.js
@@ -1,11 +1,123 @@
+let contacts =
+    [
+        {
+            "firstName": "Anna",
+            "lastName": "Schmidt",
+            "email": "annaschmidt@test123.de",
+            "phoneNumber": "01731305001",
+            "firstLetterofNames": "AS"
+        },
+        {
+            "firstName": "Bernd",
+            "lastName": "Müller",
+            "email": "berndmueller@test123.de",
+            "phoneNumber": "01731305002",
+            "firstLetterofNames": "BM"
+        },
+        {
+            "firstName": "Claudia",
+            "lastName": "Schneider",
+            "email": "claudiaschneider@test123.de",
+            "phoneNumber": "01731305003",
+            "firstLetterofNames": "CS"
+        },
+        {
+            "firstName": "David",
+            "lastName": "Fischer",
+            "email": "davidfischer@test123.de",
+            "phoneNumber": "01731305004",
+            "firstLetterofNames": "DF"
+        },
+        {
+            "firstName": "Elena",
+            "lastName": "Weber",
+            "email": "elenaweber@test123.de",
+            "phoneNumber": "01731305005",
+            "firstLetterofNames": "EW"
+        },
+        {
+            "firstName": "Felix",
+            "lastName": "Meyer",
+            "email": "felixmeyer@test123.de",
+            "phoneNumber": "01731305006",
+            "firstLetterofNames": "FM"
+        },
+        {
+            "firstName": "Greta",
+            "lastName": "Wagner",
+            "email": "gretawagner@test123.de",
+            "phoneNumber": "01731305007",
+            "firstLetterofNames": "GW"
+        },
+        {
+            "firstName": "Hans",
+            "lastName": "Becker",
+            "email": "hansbecker@test123.de",
+            "phoneNumber": "01731305008",
+            "firstLetterofNames": "HB"
+        },
+        {
+            "firstName": "Ingrid",
+            "lastName": "Schulz",
+            "email": "ingridschulz@test123.de",
+            "phoneNumber": "01731305009",
+            "firstLetterofNames": "IS"
+        },
+        {
+            "firstName": "Johannes",
+            "lastName": "Hoffmann",
+            "email": "johanneshoffmann@test123.de",
+            "phoneNumber": "01731305010",
+            "firstLetterofNames": "JH"
+        },
+        {
+            "firstName": "Karin",
+            "lastName": "Krause",
+            "email": "karinkrause@test123.de",
+            "phoneNumber": "01731305011",
+            "firstLetterofNames": "KK"
+        },
+        {
+            "firstName": "Lukas",
+            "lastName": "Lehmann",
+            "email": "lukaslehmann@test123.de",
+            "phoneNumber": "01731305012",
+            "firstLetterofNames": "LL"
+        },
+        {
+            "firstName": "Maria",
+            "lastName": "Schäfer",
+            "email": "mariaschäfer@test123.de",
+            "phoneNumber": "01731305013",
+            "firstLetterofNames": "MS"
+        },
+        {
+            "firstName": "Niklas",
+            "lastName": "Köhler",
+            "email": "niklasköhler@test123.de",
+            "phoneNumber": "01731305014",
+            "firstLetterofNames": "NK"
+        },
+        {
+            "firstName": "Olivia",
+            "lastName": "Klein",
+            "email": "oliviaklein@test123.de",
+            "phoneNumber": "01731305015",
+            "firstLetterofNames": "OK"
+        }
+    ]
+
+
+
+
 let modal = document.getElementById('contactModal');
 let btn = document.getElementById('addContactBtn');
 let span = document.getElementsByClassName('close')[0];
+
 // When the user clicks anywhere outside of the modal, close it
 function addContactModal() {
-    
-    modal.style.display = "flex";
 
+    modal.style.display = "flex";
 }
 
 
@@ -17,5 +129,21 @@ function closeModal() {
 window.onclick = function (event) {
     if (event.target == modal) {
         closeModal();
+    }
+}
+
+function init() {
+    let content = document.getElementById('contactsRenderContent');
+    content.innerHTML = '';
+    for (let contactsIndex = 0; contactsIndex < contacts.length; contactsIndex++) {
+        const contact = contacts[contactsIndex];
+        content.innerHTML += /*html*/
+            `<div class ="contact-box">
+                <div class="first-letters-of-names">${contact.firstLetterofNames}</div>
+                <div class="first-and-last-name-box">${contact.firstName} , ${contact.lastName} <br>
+                ${contact.email}
+                </div>        
+            </div>
+        `
     }
 }
