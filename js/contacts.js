85ada819c4cd808ea29ac3c50d5a801eb893e677
Winktory1304 <thomaswinkler88@googlemail.com>
2024-03-26 16:37:12 +0100

    profil bandage colors

--- js/contacts.js
+++ js/contacts.js
@@ -1,3 +1,5 @@
+let colors = ["rgb(147,39,255)", "rgb(110,82,255)", "rgb(252,113,255)", "rgb(255,195,69)", "rgb(31,215,193)", "rgb(31,215,193)", "rgb(31,215,193)", "rgb(255,70,70)", "rgb(255,122,0)", "rgb(255,122,0)"
+]
 let contacts =
     [
         {
@@ -5,105 +7,120 @@ let contacts =
             "lastName": "Schmidt",
             "email": "annaschmidt@test123.de",
             "phoneNumber": "01731305001",
-            "firstLetterofNames": "AS"
+            "firstLetterofNames": "AS",
+            "color": "rgb(147,39,255)"
         },
         {
             "firstName": "Bernd",
             "lastName": "Müller",
             "email": "berndmueller@test123.de",
             "phoneNumber": "01731305002",
-            "firstLetterofNames": "BM"
+            "firstLetterofNames": "BM",
+            "color": "rgb(110,82,255)"
         },
         {
             "firstName": "Claudia",
             "lastName": "Schneider",
             "email": "claudiaschneider@test123.de",
             "phoneNumber": "01731305003",
-            "firstLetterofNames": "CS"
+            "firstLetterofNames": "CS",
+            "color": "rgb(252,113,255)"
         },
         {
             "firstName": "David",
             "lastName": "Fischer",
             "email": "davidfischer@test123.de",
             "phoneNumber": "01731305004",
-            "firstLetterofNames": "DF"
+            "firstLetterofNames": "DF",
+            "color": "rgb(255,195,69)"
         },
         {
             "firstName": "Elena",
             "lastName": "Weber",
             "email": "elenaweber@test123.de",
             "phoneNumber": "01731305005",
-            "firstLetterofNames": "EW"
+            "firstLetterofNames": "EW",
+            "color": "rgb(147,39,255)"
         },
         {
             "firstName": "Felix",
             "lastName": "Meyer",
             "email": "felixmeyer@test123.de",
             "phoneNumber": "01731305006",
-            "firstLetterofNames": "FM"
+            "firstLetterofNames": "FM",
+            "color": "rgb(147,39,255)"
         },
         {
             "firstName": "Greta",
             "lastName": "Wagner",
             "email": "gretawagner@test123.de",
             "phoneNumber": "01731305007",
-            "firstLetterofNames": "GW"
+            "firstLetterofNames": "GW",
+            "color": "rgb(255,122,0)"
         },
         {
             "firstName": "Hans",
             "lastName": "Becker",
             "email": "hansbecker@test123.de",
             "phoneNumber": "01731305008",
-            "firstLetterofNames": "HB"
+            "firstLetterofNames": "HB",
+            "color": "rgb(147,39,255)"
         },
         {
             "firstName": "Ingrid",
             "lastName": "Schulz",
             "email": "ingridschulz@test123.de",
             "phoneNumber": "01731305009",
-            "firstLetterofNames": "IS"
+            "firstLetterofNames": "IS",
+            "color": "rgb(147,39,255)"
         },
         {
             "firstName": "Johannes",
             "lastName": "Hoffmann",
             "email": "johanneshoffmann@test123.de",
             "phoneNumber": "01731305010",
-            "firstLetterofNames": "JH"
+            "firstLetterofNames": "JH",
+            "color": "rgb(255,122,0)"
         },
         {
             "firstName": "Karin",
             "lastName": "Krause",
             "email": "karinkrause@test123.de",
             "phoneNumber": "01731305011",
-            "firstLetterofNames": "KK"
+            "firstLetterofNames": "KK",
+            "color": "rgb(147,39,255)"
         },
         {
             "firstName": "Lukas",
             "lastName": "Lehmann",
             "email": "lukaslehmann@test123.de",
             "phoneNumber": "01731305012",
-            "firstLetterofNames": "LL"
+            "firstLetterofNames": "LL",
+            "color": "rgb(147,39,255)"
         },
         {
             "firstName": "Maria",
             "lastName": "Schäfer",
             "email": "mariaschäfer@test123.de",
             "phoneNumber": "01731305013",
-            "firstLetterofNames": "MS"
+            "firstLetterofNames": "MS",
+            "color": "rgb(31,215,193)"
         },
         {
             "firstName": "Niklas",
             "lastName": "Köhler",
             "email": "niklasköhler@test123.de",
             "phoneNumber": "01731305014",
-            "firstLetterofNames": "NK"
+            "firstLetterofNames": "NK",
+            "color": "rgb(147,39,255)"
         },
         {
             "firstName": "Olivia",
             "lastName": "Klein",
             "email": "oliviaklein@test123.de",
             "phoneNumber": "01731305015",
-            "firstLetterofNames": "OK"
+            "firstLetterofNames": "OK",
+            "color": "rgb(31,215,193)"
         }
     ]
 
@@ -137,9 +154,15 @@ function init() {
     content.innerHTML = '';
     for (let contactsIndex = 0; contactsIndex < contacts.length; contactsIndex++) {
         const contact = contacts[contactsIndex];
-        content.innerHTML += /*html*/
-            `<div class ="contact-box">
-                <div class="first-letters-of-names">${contact.firstLetterofNames}</div>
+        content.innerHTML += /*html*/`
+                <div class ="contact-box">
+                <div class="first-letters-of-names">
+                    <svg width="42" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <rect width="42" height="42" rx="21" fill="white"/>
+                        <circle cx="21" cy="21" r="20" fill="${contact.color}" stroke="white" stroke-width="2"/>
+                        <text x="21" class="profil-badage" y="21" text-anchor="middle" dominant-baseline="middle" id="" fill="white">${contact.firstLetterofNames}</text>
+                    </svg>
+                </div>
                 <div class="first-and-last-name-box">${contact.firstName} , ${contact.lastName} <br>
                 <p class="conatct-box-mail">${contact.email}</p>
                 </div>        
