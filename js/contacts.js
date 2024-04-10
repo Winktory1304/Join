c8e58c3b740a92e3411325f01e2bbc6b855da784
Winktory1304 <thomaswinkler88@googlemail.com>
2024-04-10 21:48:10 +0200

    edit delete rausgenommen

--- js/contacts.js
+++ js/contacts.js
@@ -274,7 +274,7 @@ function openDetailedContactsView(contactId) {
                             <div class="detail-view-name">
                             ${contact.firstName} ${contact.lastName}
                             </div>
-                            <div class="detail-view-symbols">
+                            <!-- <div class="detail-view-symbols">
                                 <p class="edit-contact-symbols" onclick="editContact(${contactId})">
                                 <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M5 19H6.4L15.025 10.375L13.625 8.975L5 17.6V19ZM19.3 8.925L15.05 4.725L16.45 3.325C16.8333 2.94167 17.3042 2.75 17.8625 2.75C18.4208 2.75 18.8917 2.94167 19.275 3.325L20.675 4.725C21.0583 5.10833 21.2583 5.57083 21.275 6.1125C21.2917 6.65417 21.1083 7.11667 20.725 7.5L19.3 8.925ZM17.85 10.4L7.25 21H3V16.75L13.6 6.15L17.85 10.4Z" />
@@ -287,7 +287,7 @@ function openDetailedContactsView(contactId) {
                                     </svg>
                                     Delete
                                 </p>
-                            </div>
+                            </div> -->
                         </div>
                     </div>
                     <div class="detail-view-contact-information-text">Contact Information</div>
@@ -331,7 +331,10 @@ function openDetailedContactsView(contactId) {
                 <div>${contact.phoneNumber}</div>`
 }
 }
-// Popup erstellen
+
+/**
+ * Displays a popup for creating contacts.
+ */
 function createContactPopup() {
     let popup = document.getElementById('createContactsPopup');
     popup.style.display = 'flex';
@@ -420,6 +423,9 @@ function addNewContact() {
     addContact('create-contact-email-input', 'create-contact-name-input', 'create-contact-phone-input', 'contactModal');
 }
 
+/**
+ * Adds a new contact in a responsive manner.
+ */
 function addNewContactResponsiv() {
     addContact('responsivCreateContactEmailInput', 'responsivCreateContactNameInput', 'responsivCreateContactPhoneInput', 'responsivAddContact');
 }
