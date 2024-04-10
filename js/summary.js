04bb1d740b7e2207c3abb7df8a55f24ae8d2e1bb
Artur Borowik <ArturBorowik@gmx.net>
2024-04-10 20:58:58 +0200

    changes responsive

--- js/summary.js
+++ js/summary.js
@@ -85,6 +85,7 @@ function showAllNumbers() {
     showNumberOfAwaitingFeedback();
     showWayOfGreeting()
     getName();
+    showUrgentToDo();
 }
 
 
@@ -141,7 +142,43 @@ function UpcomingDeadline() {
         .map(todo => todo.date);
 
     console.log(filteredDates);
+}
 
+/**
+ * Shows the amout of urgent todos and the nearest due date of an urgent todo.
+ */
+function showUrgentToDo() {
+    let urgent = document.getElementById('number_of_urgent');
+    let dateDiv = document.getElementById('summary_urgent_date');
+    
+    let todo = todos.filter(t => t['priority'] == 3 && t['status'] == 'open');
+    
+    if (todo.length === 0) {
+        urgent.innerHTML = 0;
+        dateDiv.innerHTML = "No urgent tasks"; //*** Display if no urgent tasks are found*/
+    } else {
+        let sortedTasks = todo.sort((a, b) => new Date(a.date) - new Date(b.date));
+        let date = new Date(sortedTasks[0].date);
+        date = formatDate(date);
+        dateDiv.innerHTML = date;
+        urgent.innerHTML = todo.length;
+    }
+}
 
+/**
+ * Formats the date into a custom format.
+ * @param {Date} date - The date to format.
+ * @returns {string} The formatted date string.
+ */
+function formatDate(date) {
+    const months = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
 
+    const day = date.getDate();
+    const monthIndex = date.getMonth();
+    const year = date.getFullYear();
+
+    return `${months[monthIndex]} ${day}, ${year}`;
 }
--- js/template.js
+++ js/template.js
@@ -12,3 +12,23 @@ function showHeaderMenu(headerID, hideHeaderClass) {
         isShowMenu = true;
     }
 }
+
+
+window.addEventListener('resize', responsive);
+responsive();
+
+function responsive() {
+    const screenWidth = window.innerWidth;
+    const thresholdWidth = 596;
+    const targetElement = document.getElementById('join_logo_mobile');
+
+    if (screenWidth <= thresholdWidth) {
+        // Screen width greater than 596px
+        targetElement.classList.remove('d-none');
+
+    } else {
+        // Screen width less than or equal to 596px
+        targetElement.classList.add('d-none');
+    }
+}
+
