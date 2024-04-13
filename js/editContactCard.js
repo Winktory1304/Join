270d01b3f80584af915fb48e24ca09a35478d111
Winktory1304 <thomaswinkler88@googlemail.com>
2024-04-13 10:21:54 +0200

    delete function

--- js/editContactCard.js
+++ js/editContactCard.js
@@ -176,8 +176,8 @@ function editContact() {
                     type="number" placeholder="Phone">
             </form>
             <div class="modal-part-3-buttons-container">
-                <button class="modal-part-3-cancel-button" onclick="hideModal('editContactCard')">
-                    Cancel
+                <button class="modal-part-3-cancel-button" onclick="hideModal('editContactCard'); deleteContactById(${contactId})">
+                    Delete
                     <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M12.001 12.5001L17.244 17.7431M6.758 17.7431L12.001 12.5001L6.758 17.7431ZM17.244 7.25708L12 12.5001L17.244 7.25708ZM12 12.5001L6.758 7.25708L12 12.5001Z"
