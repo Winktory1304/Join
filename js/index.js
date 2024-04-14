528ba975e5fc73d1ece04af11c8dcff32afae025
Artur Borowik <ArturBorowik@gmx.net>
2024-04-14 18:56:10 +0200

    responsive summary

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ function registerWindow() {
     document.getElementById('log_in_container').classList.add('d-none');
     document.getElementById('sing_up_container').classList.remove('d-none');
     document.getElementById('log_container').classList.add('height-sing-up');
+    
     resetForm();
     hideSignUpHeader();
 }
