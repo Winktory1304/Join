7e9392b7f9a8faa59d5eaba8351a83be20f91207
Christina Bernhardt <sozia76@web.de>
2024-04-14 22:52:00 +0200

    debugger

--- js/add_task.js
+++ js/add_task.js
@@ -285,7 +285,6 @@ function checkId() {
  * @returns {boolean} - The validation result.
  */
 function validateForm() {
-  debugger
   if (switchCase('titleValue')  !== "" && switchCase('dateValue')  !== "" && switchCase('categoryValue')  !== "") {
     return true;
   } else {
